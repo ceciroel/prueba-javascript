b2ae604cfceb7354a20e6b9be28e0225255c2c45
ceciroel <kcc.roel@gmail.com>
2016-06-14 21:37:08 -0500

    Add files via upload

---
+++ p1/script.js
@@ -0,0 +1,17 @@
+var suma=0;
+do {
+var ingreso1 = prompt("Ingrese un número");
+} while (ingreso1 != isNaN("fin"));
+do {
+var ingreso2 = prompt("Ingrese un número");
+} while (ingreso2 != isNaN("fin"));
+alert(ingreso1 + ingreso2);
+
+
+
+
+var suma=0;
+do{
+	var ingreso = prompt("ingresa un numero");
+} while(isNaN(ingreso));
+console.log(suma);
